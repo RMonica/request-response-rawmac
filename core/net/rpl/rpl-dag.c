ff23975dca22fa2a4c998fd1b09bfb8fbbe90224
user <user@ubuntu>
2013-03-25 11:22:32 +0100

    debug

--- core/net/rpl/rpl-dag.c
+++ core/net/rpl/rpl-dag.c
@@ -52,8 +52,8 @@
 #include <limits.h>
 #include <string.h>
 
-//#define DEBUG DEBUG_NONE
-#define DEBUG 2
+#define DEBUG DEBUG_NONE
+//#define DEBUG 2
 //#define DEBUG DEBUG_PRINT
 #include "net/uip-debug.h"
 
